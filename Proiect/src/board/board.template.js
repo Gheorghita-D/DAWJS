80e4c434d54708cbb651247f78358a8f5c1fbbbb
georgep98 <48506690+georgep98@users.noreply.github.com>
2020-01-02 17:11:39 +0200

    Add files via upload

---
+++ Proiect/src/board/board.template.js
@@ -0,0 +1,19 @@
+export const board = `<section>
+    <main>
+        <article id="lists">
+            <div class="list"><label for="noCategory">No category</label>
+                <ul id="noCategory"></ul>
+            </div><button>&#x2b;</button>
+        </article>
+    </main>
+    <aside>
+    </aside>
+    <div class="modal">
+        <div class="modalContent">
+            <label class='close'>&times;</label>
+            <input class='newCategory' placeholder="Category name">
+        </div>
+    </div>
+
+
+</section>`
