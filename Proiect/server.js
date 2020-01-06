6b031695ba65b583ef2ffdaa266ac70428578fe7
alexandragiit <alexandra@birzu.ro>
2020-01-06 17:24:32 +0200

    se adauga in baza de date emailul si parola la inregistrare

--- Proiect/server.js
+++ Proiect/server.js
@@ -21,7 +21,32 @@ router.get('/register',function(req,res){
 	res.sendFile(path.join(__dirname+'/index.html'));
 });
 
+ /*-- adauga in baza de date userul la inregistrare --*/
+const model = require('./model.js');
+router.use(express.urlencoded());
+router.use(express.json());      // if needed
+router.post('/register', function(request,response){
 
+	var email = request.body.email;
+	var password = request.body.psw;
+
+	var newuser = new model.Users();
+	newuser.username = email;
+	newuser.password = password;
+	newuser.id = '123345';
+
+	newuser.save(function(err, savedUser){
+		if(err){
+			console.log(err);
+			return response.status(500).send();
+		}
+		return response.sendFile(path.join(__dirname+'/index.html'));
+	});
+	
+
+	response.sendFile(path.join(__dirname+'/index.html'));
+});
+/* --end-- */
 
 router.post('/tasks',function(req,res){
 	console.log("ihhkkjg")
