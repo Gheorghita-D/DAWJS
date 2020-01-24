685be9a892b3245b864727274611d561cd0e3124
alexandragiit <alexandra@birzu.ro>
2020-01-24 12:43:40 +0200

    adaugat register and login

--- proiect-with-templates/server.js
+++ proiect-with-templates/server.js
@@ -3,6 +3,12 @@ const app = express();
 const model = require('./model.js'); 
 const mongoose = require('mongoose');
 
+
+var jwt = require('jsonwebtoken');
+var bcrypt = require('bcryptjs');
+
+
+
 const PORT = 3000;
 app.listen(PORT); // change from app
 console.log('Running at Port 3000');
@@ -54,26 +60,26 @@ app.get('/register',function(req,res){
  /*-- adauga in baza de date userul la inregistrare --*/
 app.use(express.urlencoded());
 app.use(express.json());      // if needed
-app.post('/register', function(request,response){
+// app.post('/register', function(request,response){
 
-	var email = request.body.email;
-	var password = request.body.psw;
+// 	var email = request.body.email;
+// 	var password = request.body.psw;
 
-	var newuser = new model.Users();
-	newuser.username = email;
-	newuser.password = password;
-	newuser.id = '123345'; // trebuie pus random id
+// 	var newuser = new model.Users();
+// 	newuser.username = email;
+// 	newuser.password = password;
+// 	newuser.id = '123345'; // trebuie pus random id
 
-	newuser.save(function(err, savedUser){
-		if(err){
-			console.log("eroare: " + err);
-			return response.status(500).send();
-		}
+// 	newuser.save(function(err, savedUser){
+// 		if(err){
+// 			console.log("eroare: " + err);
+// 			return response.status(500).send();
+// 		}
 		
-		return response.render('index', {data});
+// 		return response.render('index', {data});
 	
-	});
-});
+// 	});
+// });
 /* --end-- */
 
 app.post('/tasks',function(req,res){
@@ -180,3 +186,55 @@ app.post('/addproject', function(req,res){
 
 })
 
+
+app.post('/register', function (req, res) {
+	var id = mongoose.Types.ObjectId();
+
+	var hashedPassword = bcrypt.hashSync(req.body.psw, 8);
+
+	model.Users.create({
+		username: req.body.email,
+		// email: req.body.email,
+		password: hashedPassword,
+	  	id: id,
+	},
+		function (err, user) {
+			if (err) return res.status(500).send("There was a problem registering the user.")
+			// create a token
+			// var token = jwt.sign({ id: user._id }, config.secret, {
+			// 	expiresIn: 86400 // expires in 24 hours
+			// });
+			// res.status(200).send('Successfully registered!');
+			data = {page:"login"};
+			res.render('index', {data});
+			//cookie
+		});
+});
+var authData;
+app.post('/login', function(req, res) {
+
+	model.Users.findOne({ username : req.body.uname }, function (err, user) {
+	  if (err) return res.status(500).send('Error on the server.');
+	  if (!user) return res.status(404).send('No user found.');
+	  
+	  var passwordIsValid = bcrypt.compareSync(req.body.psw, user.password);
+	  if (!passwordIsValid) return res.status(401).send({ auth: false, token: null });
+	  
+	  var token = jwt.sign({ id: user._id }, "123456789", {
+		expiresIn: 86400 // expires in 24 hours
+	  });
+	  
+	//   res.status(200).send({ auth: true, token: token, user: user.id });
+	
+	  authData = {
+		auth: true, token: token, user: user.id
+	  }
+	  data = {page:"projects",authData: authData}
+	  res.render('index', {data});
+	});
+	
+  });
+
+  app.get("/token", function(req, res){
+	res.send({authData})
+  })
